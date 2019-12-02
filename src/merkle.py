f1352b65ac491be9656f6c2e6c3833fcb2dbe3c7
Kaitlin Walsh <klwalsh@bu.edu>
2019-12-01 23:16:00 -0500

    updated to include adding by list

--- src/merkle.py
+++ src/merkle.py
@@ -1,75 +1,139 @@
+# Isolated testing script for Merkle Tree functions
+# Functionality is tested here before being integrated with
+# the Keylime Provider Verifier
 import string
 import hashlib
+import json
+import io
 
-from merklelib import MerkleTree, beautify
+from merklelib import MerkleTree, beautify, export
 from string import ascii_lowercase
 
 # Define custom hash function
-# May just need "return value" in our implementation
 def hashfunc(value):
-  # Convert to string because it doesn't like bytes
-  new_value = str(value)
-  hash = hashlib.sha256(new_value.encode('utf-8')).hexdigest()
-  return hash
+  new_value = value
+  return new_value
 
+# --------------- LEAF APPEND / TREE GENERATE TEST -------------------- #
+print("\n--------------- LEAF APPEND / TREE GENERATE TEST --------------------")
+# This test simulates accumulating nonces one at a time
 # Instantiate empty list
 data = []
 
-# Instantiate empty Merkle tree
-tree = MerkleTree(data, hashfunc)
+# Instantiate empty Merkle tree for appending
+asciiTree = MerkleTree(data, hashfunc)
 
 # Add all ASCII characters to Merkle tree
 for i in ascii_lowercase:
-  tree.append(i.encode('utf-8'))
+  asciiTree.append(i.encode('utf-8'))
 
 # Print MerkleTree
-print("\n")
-beautify(tree)
+print("\nPrinting Merkle Tree")
+beautify(asciiTree)
+print("\nPrinting root")
+print(asciiTree.merkle_root)
+print("\nPrinting leaves")
+print(asciiTree.leaves)
 
 # Generate an audit proof the letter a
-print("\n")
-print("Generating audit proof for a")
-print("Hash of a is : " + hashlib.sha256("a".encode('utf-8')).hexdigest())
-proof = tree.get_proof('a')
-print("Proof: ")
+print("\nGenerating audit proof for a")
+proof = asciiTree.get_proof('a')
+print("\nProof: ")
 print(proof)
-print()
 
 # Verify that a is in the tree
-if tree.verify_leaf_inclusion('a', proof):
-  print('a is in the tree')
-  print()
+if asciiTree.verify_leaf_inclusion('a', proof):
+  print('\na is in the tree')
 else:
   exit('a is not in the tree')
 
-# Add in string hash to test
+# Add in longer string hash to test
 new_hash = "rgie3948guhiev"
-tree.append(new_hash)
+asciiTree.append(new_hash)
 
 # Generate an audit proof for the string
-print("Generating audit proof for rgie3948guhiev")
-proof = tree.get_proof('rgie3948guhiev')
-print("Proof: ")
+print("\nGenerating audit proof for rgie3948guhiev")
+proof = asciiTree.get_proof('rgie3948guhiev')
+print("\nProof: ")
 print(proof)
-print()
 
 # Verify that rgie3948guhiev is in the tree
-if tree.verify_leaf_inclusion('rgie3948guhiev', proof):
-  print('rgie3948guhiev is in the tree')
-  print()
+if asciiTree.verify_leaf_inclusion('rgie3948guhiev', proof):
+  print('\nrgie3948guhiev is in the tree')
 else:
   exit('rgie3948guhiev is not in the tree')
 
-# Generate an audit proof for random string
-print("Generating audit proof for DONTFINDME")
-proof = tree.get_proof('DONTFINDME')
-print("Proof: ")
+# Generate an audit proof for random string NOT in the tree
+print("\nGenerating audit proof for DONTFINDME")
+proof = asciiTree.get_proof('DONTFINDME')
+print("\nProof: ")
 print(proof)
-print()
 
 # Verify that DONTFINDME is not in the tree
-if tree.verify_leaf_inclusion('DONTFINDME', proof):
-  print('DONTFINDME is in the tree')
-  print()
+if asciiTree.verify_leaf_inclusion('DONTFINDME', proof):
+  exit('\nDONTFINDME is in the tree')
 else:
-  exit('DONTFINDME is not in the tree')
+  print('\nDONTFINDME is not in the tree')
+
+print("\n--------------- LEAF APPEND / TREE GENERATE TEST END --------------------")
+# --------------- LEAF APPEND / TREE GENERATE TEST END -------------------- #
+
+
+# --------------- GENERATE FROM LIST TEST -------------------- #
+print("\n--------------- GENERATE FROM LIST TEST --------------------")
+# This test simulates accumulating nonces in a single list
+# Instantiate empty list
+data = []
+
+# Initialize testing list
+newData = ["aoibune", "24pot309grtjb", "poegijshbn", "oqfh8vyu", "0298f7gvublnk", "pq09483f7guvbl"]
+
+# Instantiate empty Merkle tree for appending
+listTree = MerkleTree(data, hashfunc)
+
+# Add list data to merkle tree
+listTree.extend(newData);
+
+# Print MerkleTree
+print("\nPrinting Merkle Tree")
+beautify(listTree)
+print("\nPrinting root")
+print(listTree.merkle_root)
+print("\nPrinting leaves")
+print(listTree.leaves)
+
+# Generate an audit proof for the string
+print("\nGenerating audit proof for poegijshbn")
+proof = listTree.get_proof('poegijshbn')
+print("\nProof: ")
+print(proof)
+
+# Verify that rgie3948guhiev is in the tree
+if listTree.verify_leaf_inclusion('poegijshbn', proof):
+  print('\npoegijshbn is in the tree')
+else:
+  exit('poegijshbn is not in the tree')
+
+# Generate an audit proof for random string NOT in the tree
+print("\nGenerating audit proof for DONTFINDME")
+proof = listTree.get_proof('DONTFINDME')
+print("\nProof: ")
+print(proof)
+
+# Verify that DONTFINDME is not in the tree
+if listTree.verify_leaf_inclusion('DONTFINDME', proof):
+  exit('\nDONTFINDME is in the tree')
+else:
+  print('\nDONTFINDME is not in the tree')
+
+print("\n--------------- GENERATE FROM LIST TEST END --------------------")
+# --------------- GENERATE FROM LIST TEST END -------------------- #
+
+# --------------- EXPORT TREE TEST -------------------- #
+# Jsonify and json.dumps fail because the object MerkleTree is not serializable
+
+# This doesn't work currently
+# NameError: name 'io' is not defined
+export(asciiTree, filename='ascii', ext='json')
+
+# --------------- EXPORT TREE TEST END -------------------- #

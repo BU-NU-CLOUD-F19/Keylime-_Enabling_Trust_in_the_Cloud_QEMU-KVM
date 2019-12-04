300368cb131a0f400ecb33d3ebe0496fc1007b48
Kaitlin Walsh <klwalsh@bu.edu>
2019-12-03 19:04:01 -0500

    update for nonce verification

--- src/merklefunctions.py
+++ src/merklefunctions.py
@@ -9,39 +9,43 @@ import sys
 # This needs to be be hashlib.sha1(str(value).encode()).hexdigest()
 # if using in Keylime
 def hashfunc(value):
-	hash = hashlib.sha256(str(value).encode()).hexdigest()
+	hash = hashlib.sha1(str(value).encode()).hexdigest()
 	return hash
 
 def proof_to_string(proof):
-	hashlist, typelist = proof_to_lists(proof)
-	hash_string = '%s' % ','.join(map(str, hashlist))
-	type_string = '%s' % ','.join(map(str, typelist))
-	proof_string = hash_string + ':' + type_string
-	# Return value is string of node info delimited by :
-	# Node info is delimited by ,
-	return proof_string
+    hashlist, typelist = proof_to_lists(proof)
+    hash_string = '%s' % ','.join(map(str, hashlist))
+    type_string = '%s' % ','.join(map(str, typelist))
+    proof_string = hash_string + ':' + type_string
+    # Return value is string of node info delimited by :
+    # Node info is delimited by ,
+    return proof_string
 
 def proof_to_lists(proof):
-	hashlist = []
-	typelist = []
-	for node in proof._nodes:
-		hashlist.append(utils.to_hex(node.hash))
-		typelist.append(node.type)
-	return hashlist, typelist
+    hashlist = []
+    typelist = []
+    for node in proof._nodes:
+        hashlist.append(utils.to_hex(node.hash))
+        typelist.append(node.type)
+    return hashlist, typelist
 
 def lists_to_proof(hashlist, typelist):
-	nodepath = []
-	for nodehash, nodetype in zip(hashlist, typelist):
-		nodepath.append(merklelib.AuditNode(utils.from_hex(nodehash), nodetype))
-	proof = merklelib.AuditProof(nodepath)
-	return proof
+    nodepath = []
+    for nodehash, nodetype in zip(hashlist, typelist):
+        nodepath.append(merklelib.AuditNode(utils.from_hex(nodehash), nodetype))
+    proof = merklelib.AuditProof(nodepath)
+    return proof
 
 def string_to_proof(proof_string):
-	hashstring, typestring = proof_string.split(":")
-	hashlist = hashstring.split(",")
-	typelist = typestring.split(",")
-	proof = lists_to_proof(hashlist, typelist)
-	return proof
+    hashstring, typestring = proof_string.split(":")
+    if not hashstring:
+        hashlist = []
+        typelist = []
+    else:
+        hashlist = hashstring.split(",")
+        typelist = typestring.split(",")
+    proof = lists_to_proof(hashlist, typelist)
+    return proof
 
 def main():
 	data = []
@@ -61,13 +65,20 @@ def main():
 	remade_proof = lists_to_proof(proof_hashs, proof_types)
 	print(merklelib.verify_leaf_inclusion('a', remade_proof, hashfunc, utils.to_hex(root.hash)))
 
+	data2 = []
+	tree2 = MerkleTree(data2, hashfunc)
+	tree2.append('a')
+	root2 = tree2._root
+	original_proof2 = tree2.get_proof('a')
+	print(original_proof2)
+
 	print("Proof to string test")
-	string_proof = proof_to_string(original_proof)
+	string_proof = proof_to_string(original_proof2)
 	print(type(string_proof) == type("hello"))
 
 	print("Proof to string/ String to proof verify_leaf_inclusion")
 	new_proof = string_to_proof(string_proof)
-	print(merklelib.verify_leaf_inclusion('a', remade_proof, hashfunc, utils.to_hex(root.hash)))
+	print(merklelib.verify_leaf_inclusion('a', new_proof, hashfunc, utils.to_hex(root2.hash)))
 
 if __name__ == '__main__':
   test = 'b'
